fdf45e3f130720da75ea1845ef1804b971e43988
matheusor99 <matheusor99@gmail.com>
2020-02-14 10:41:41 -0300

    ultimas validações

--- src/main/java/com/example/api/service/EnderecoService.java
+++ src/main/java/com/example/api/service/EnderecoService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -39,10 +40,16 @@ public class EnderecoService {
 		try {
 			Customer customer = customerService.findById(idCustomer).get();
 			Endereco endereco = search(cep);
-			customer.addEndereco(endereco);
-			endereco.setCustomer(customer);
-			Endereco result = repository.save(endereco);
-			return result;
+			List<Endereco> validacaoEndercoExistente = customer.getEnderecos().stream()
+					.filter((e) -> e.getCep().equals(endereco.getCep())).collect(Collectors.toList());
+			if (validacaoEndercoExistente.isEmpty()) {
+				customer.addEndereco(endereco);
+				endereco.setCustomer(customer);
+				Endereco result = repository.save(endereco);
+				return result;
+			} else {
+				return validacaoEndercoExistente.get(0);
+			}
 		} catch (DomainExceptions e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
@@ -53,6 +60,7 @@ public class EnderecoService {
 	
 	public String delete(Long id) {
 		if (repository.findById(id).get() != null) {
+			repository.deleteById(id);
 			return "Endereço excluido com sucesso";
 		}
 		return "Endereço inexistente";
